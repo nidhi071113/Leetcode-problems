b9a0104f74e31cb7569d71a153a7b3e3cf5423a2
nidhi071113 <nidhishirsath0713@gmail.com>
2026-08-17 23:24:37 +0530

    Time: 403 ms (66.93%), Space: 28.2 MB (16.04%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.cpp
+++ 1563-stone-game-v/1563-stone-game-v.cpp
@@ -1,39 +1,60 @@
 class Solution {
 public:
-    vector<vector<int>> memo;
-    vector<int> pre;
 
-    int solve(int i, int j) {
-        if (i == j) return 0;
-        if (memo[i][j] != -1) return memo[i][j];
+    int solver(vector<int>& prefsum , int i , int j , vector<vector<int>>&dp) {
 
-        int total = pre[j + 1] - pre[i];
-        int best = 0;
+        if (i==j) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
 
+        int cost = 0 , maxcost = INT_MIN;
         for (int k = i; k < j; k++) {
-            int left = pre[k + 1] - pre[i];
-            int right = total - left;
+
+            int left  = prefsum[k] - prefsum[i-1];
+            int right = prefsum[j] - prefsum[k];
 
             if (left < right) {
-                best = max(best, left + solve(i, k));
-            } else if (left > right) {
-                best = max(best, right + solve(k + 1, j));
-            } else {
-                best = max(best, left + max(solve(i, k), solve(k + 1, j)));
+
+                // Bob removes right, Alice keeps left
+                cost = left + solver(prefsum, i, k, dp);
+
+            } 
+            else if (left > right) {
+
+                // Bob removes left, Alice keeps right....in case of 
+                cost = right + solver(prefsum, k+1, j, dp);
+
+            } 
+            else {
+
+                // Equal -> Alice can choose either of the sides 
+                cost = left + max(
+                    solver(prefsum, i, k, dp),
+                    solver(prefsum, k+1, j, dp)
+                );
             }
+
+            maxcost = max(maxcost, cost);
         }
 
-        return memo[i][j] = best;
+        return dp[i][j] = maxcost;
+
     }
 
-    int stoneGameV(vector<int>& stoneValue) {
-        int n = stoneValue.size();
-        pre.assign(n + 1, 0);
-        for (int i = 0; i < n; i++) {
-            pre[i + 1] = pre[i] + stoneValue[i];
+    int stoneGameV(vector<int>& stones) {
+        
+        //dp[i][j] tells us the maximum sum alice can get from subarray i,j//
+        int n = stones.size();
+        vector<int> prefsum;
+        int sum = 0;
+        prefsum.push_back(0);
+        for (auto it:stones) {
+            sum += it;
+            prefsum.push_back(sum);
         }
 
-        memo.assign(n, vector<int>(n, -1));
-        return solve(0, n - 1);
+        vector<vector<int>> dp(n+2 , vector<int>(n+2 , -1));
+
+        return solver(prefsum , 1 , n, dp);
+
     }
 };
