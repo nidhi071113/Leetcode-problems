ca785a952ff0d87af84d55c5c09b2af90448552d
nidhi071113 <nidhishirsath0713@gmail.com>
2026-09-01 18:28:53 +0530

    Time: 0 ms (100%), Space: 37.2 MB (82.71%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -11,15 +11,35 @@
  */
 class Solution {
 public:
-    unordered_set<int> seen;
+    // unordered_set<int> seen;
+
+    // bool findTarget(TreeNode* root, int k) {
+    //     if (!root) return false;
+
+    //     if (seen.count(k - root->val)) return true;
+
+    //     seen.insert(root->val);
+
+    //     return findTarget(root->left, k) || findTarget(root->right, k);
+    // }
+
+    void inorder(TreeNode* root,vector<int> &res){
+        if(root == NULL) return;
+        inorder(root->left,res);
+        res.push_back(root->val);
+        inorder(root->right,res);
+    }
 
     bool findTarget(TreeNode* root, int k) {
-        if (!root) return false;
-
-        if (seen.count(k - root->val)) return true;
-
-        seen.insert(root->val);
-
-        return findTarget(root->left, k) || findTarget(root->right, k);
+        vector<int> res;
+        inorder(root,res);
+        int left = 0 , right = res.size() - 1;
+        while(left < right){
+            int sum = res[left] + res[right];
+            if(sum == k) return true;
+            if(sum < k) left++;
+            else right--;
+        }
+        return false;
     }
 };
