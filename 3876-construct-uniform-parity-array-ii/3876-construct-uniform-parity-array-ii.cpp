599b9f1f1a10d8f5a738b0138f29471c8e33c18b
nidhi071113 <nidhishirsath0713@gmail.com>
2026-09-03 13:48:57 +0530

    Time: 6 ms (52.36%), Space: 165.8 MB (87.43%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int mini = INT_MAX;
+        for(int x : nums1){
+            if(x % 2 == 1) mini = min(mini,x);
+        }   
+        for(int x : nums1){
+            if(x % 2 == 0 && mini != INT_MAX && x < mini) return false;
+        }
+        return true;
+    }
+};
