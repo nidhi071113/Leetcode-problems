ab0df32f7feeb9c2a8a400ec8d189b88c5da8da9
nidhi071113 <nidhishirsath0713@gmail.com>
2026-09-08 09:36:38 +0530

    Time: 23 ms (22.28%), Space: 17.2 MB (38.41%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        // Initialize the distance matrix with INT_MAX
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        
+        // Populate direct edge weights
+        for (auto it : edges) {
+            dist[it[0]][it[1]] = it[2];
+            dist[it[1]][it[0]] = it[2];
+        }
+        
+        // Distance to self is always 0
+        for (int i = 0; i < n; i++) {
+            dist[i][i] = 0;
+        }
+
+        // Floyd-Warshall Algorithm to find all-pairs shortest paths
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][k] == INT_MAX || dist[k][j] == INT_MAX) 
+                        continue;
+                    
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+
+        int cntCity = n;
+        int cityNo = -1;
+
+        // Count reachable cities within threshold for each city
+        for (int city = 0; city < n; city++) {
+            int cnt = 0;
+            for (int adjCity = 0; adjCity < n; adjCity++) {
+                if (dist[city][adjCity] <= distanceThreshold) {
+                    cnt++;
+                }
+            }
+
+            // Pick city with smallest count (or larger index in case of a tie)
+            if (cnt <= cntCity) {
+                cntCity = cnt;
+                cityNo = city;
+            }
+        }
+
+        return cityNo;
+    }
+};
