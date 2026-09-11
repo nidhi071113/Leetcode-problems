9120c2152aa5955b690e7f7b3f7357a3450bd83a
nidhi071113 <nidhishirsath0713@gmail.com>
2026-09-11 17:46:32 +0530

    Time: 29 ms (68.72%), Space: 40.3 MB (68.16%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -0,0 +1,84 @@
+class DisjointSet {
+public:
+    vector<int> rank, parent, size;
+
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1, 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    // Find operation with Path Compression
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    // Union by Size
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v) return;
+
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        } else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        int n = accounts.size();
+        DisjointSet ds(n);
+        unordered_map<string, int> mapMailNode;
+
+        // Step 1: Map each mail to its node index and perform Union for duplicates
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < accounts[i].size(); j++) {
+                string mail = accounts[i][j];
+                
+                if (mapMailNode.find(mail) == mapMailNode.end()) {
+                    mapMailNode[mail] = i;
+                } else {
+                    ds.unionBySize(i, mapMailNode[mail]);
+                }
+            }
+        }
+
+        // Step 2: Group emails under their ultimate parent component
+        vector<vector<string>> mergedMail(n);
+        for (auto it : mapMailNode) {
+            string mail = it.first;
+            int node = ds.findUPar(it.second);
+            mergedMail[node].push_back(mail);
+        }
+
+        // Step 3: Format the result with account names and sorted email lists
+        vector<vector<string>> ans;
+        for (int i = 0; i < n; i++) {
+            if (mergedMail[i].empty()) continue;
+
+            sort(mergedMail[i].begin(), mergedMail[i].end());
+
+            vector<string> temp;
+            temp.push_back(accounts[i][0]); // Name of the account
+            for (auto it : mergedMail[i]) {
+                temp.push_back(it);
+            }
+
+            ans.push_back(temp);
+        }
+
+        return ans;
+    }
+};
